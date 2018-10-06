a33fa82069d315bc88c542ac7e595e0af640949b
Srikant <rathoresrikant@gmail.com>
2018-10-06 14:14:02 +0530

    Selection Sort
    Added the code for selection sort in C++.

---
+++ C++/SELECTIONSORT.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[15],n=10,temp;
+	for(int i = 0;i<n;i++)
+	cin>>arr[i];
+	for(int j = 0;j<n-1;j++)
+	{
+	    int minimum = j;
+	    for(int k = j+1;k<n;k++)
+	    {
+	        if(arr[k]<arr[minimum])
+	        {
+	            temp = arr[k];
+	            arr[k] = arr[minimum];
+	            arr[minimum] = temp;
+	        }
+	    }
+	}
+	for(int i = 0;i<n;i++)
+	cout<<"\t"<<arr[i];
+}
