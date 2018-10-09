5a85393e0e703a19cab52a4604cb4ce9082bf37f
Findsen <chr.findsen@gmail.com>
2018-10-09 14:40:29 +0200

    Create Counter.java

---
+++ Java/Counter.java
@@ -0,0 +1,10 @@
+public class Counter{
+  public static void main(String[] args){
+  counter(50);
+  }
+  private void counter(int numbers){
+    for(i = 0; i <0 numbers; i++ ){
+      System.out.println("This is number: "+i);
+    }
+  }
+}
